6d056b41c6185d4bf4345d752c9cd7e1b5da2800
Tatikonda Rahul <74859747+Rahulisunq@users.noreply.github.com>
2020-12-03 18:08:08 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,6 @@
+d={1:'ravi',2:'raju',3:'sudheer',4:'sekhar'}
+print("1st name is"+d[1])
+print("2nd name is"+d[2])
+print(d)
+print(d.keys())
+print(d.values())
---
+++ mam.py
@@ -0,0 +1,62 @@
+print("press 1 for arithmetic operator")
+print("press 2 for relational operator")
+print("press 3 for logical operator")
+print("press 4 for bitwise operator")
+print("press 5 for assignment operator")
+print("press 6 for special operator")
+print("press 7 for membership operator")
+x=int(input("enter the number of operator set that you want to apply: "))
+if(x==1):
+	a=int(input("enter the value of a: "))
+	b=int(input("enter the value of b: "))
+	print("addition of two number is: ",(a+b))
+	print("subtraction of two number is: ",(a-b))
+	print("multiplication of two number is: ",(a*b))
+	print("division of two number is: ",(a/b))
+	print("modulus of two number is: ",(a%b))
+	print("power function applied between two number is: ",(a**b))
+	print("floor division applied for two number is: ",(a//b))
+elif(x==2):
+	a=int(input("enter the value of a: "))
+	b=int(input("enter the value of b: "))
+	print(a>b)
+	print(a<b)
+	print(a>=b)
+	print(a<=b)
+	print(a==b)
+	print(a!=b)
+elif(x==3):
+	a=input("type either true or false: ")
+	b=input("type either true or false: ")
+	print(a and b)
+	print(a or b)
+	print(not a)
+	print(not b)
+elif(x==4):
+	a=int(input("enter the value of a: "))
+	b=int(input("enter the value of b: "))
+	print(a&b)
+	print(a/b)
+	print(~a)
+	print(~b)
+	print(a^b)
+	print(b^a)
+	print(a>>2)
+	print(a<<2)
+elif(x==5):
+	a=int(input("enter the value of a: "))
+	a+=2
+	print("sum ",a)
+	a-=2
+	print("difference ",a)
+	a*=2
+	print("multiplication ",a)
+	a/=2
+	print("division ",a)
+	a//=2
+	print("floor division ",a)
+	a**=2
+	print("power function ",a)
+	a%=2
+	print("modulus ",a)
+	
